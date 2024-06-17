39bb42fd661f1ca8cc39b7410bc3ad648a443f2c
EishaV <30573296+EishaV@users.noreply.github.com>
2024-06-17 15:32:21 +0200

    landscape für Status

--- AvaDeskApp/Views/StatusTab.axaml.cs
+++ AvaDeskApp/Views/StatusTab.axaml.cs
@@ -5,5 +5,16 @@ namespace AvaApp.Views {
     public StatusTab() {
       InitializeComponent();
     }
+    protected override void OnSizeChanged(SizeChangedEventArgs e) {
+      base.OnSizeChanged(e);
+
+      if( e.NewSize.Width < e.NewSize.Height ) {
+        StatusLow[Grid.ColumnProperty] = 0;
+        StatusLow[Grid.RowProperty] = 1;
+      } else {
+        StatusLow[Grid.ColumnProperty] = 1;
+        StatusLow[Grid.RowProperty] = 0;
+      }
+    }
   }
 }
